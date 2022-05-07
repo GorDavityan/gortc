a4fc5e3f153c0ce09329bb4df5698c219f9a9f38
Gor Davityan <gordavityan@Gors-MacBook-Air.local>
2022-05-07 23:43:25 +0400

    Route table and associations

--- main.tf
+++ main.tf
@@ -56,3 +56,41 @@ resource "aws_subnet" "PUBLIC-SUBNET-1c" {
       Name = "Trazler PUBLIC-SUBNET-1c"
   }
 }
+
+#********************************************************#
+
+resource "aws_route_table" "PUBLIC-ROUTE-TABLE" {
+   vpc_id = aws_vpc.VPC.id
+
+   route = {
+     cidr_block = "0.0.0.0/0"
+     gateway_id = aws_internet_gateway.IGW.id
+     } 
+
+    tags = {
+        Name = "Trazler PUBLIC ROUTE TABLE"
+    }  
+ }
+ 
+#********************************************************#
+
+resource "aws_route_table_association" "PUBLIC-SUBNET-1a-ROUTE-TABLE-ASSOCIATION" {
+  subnet_id = aws_subnet.PUBLIC-SUBNET-1a.id
+  route_table_id = aws_route_table.PUBLIC-ROUTE-TABLE.id
+}
+
+#********************************************************#
+
+resource "aws_route_table_association" "PUBLIC-SUBNET-1a-ROUTE-TABLE-ASSOCIATION" {
+  subnet_id = aws_subnet.PUBLIC-SUBNET-1b.id
+  route_table_id = aws_route_table.PUBLIC-ROUTE-TABLE.id
+}
+
+#********************************************************#
+
+resource "aws_route_table_association" "PUBLIC-SUBNET-1a-ROUTE-TABLE-ASSOCIATION" {
+  subnet_id = aws_subnet.PUBLIC-SUBNET-1c.id
+  route_table_id = aws_route_table.PUBLIC-ROUTE-TABLE.id
+}
+
+

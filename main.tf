52e727663ff04cdefc8c96b2761c5b0cb047150e
Gor Davityan <gordavityan@Gors-MacBook-Air.local>
2022-05-07 23:06:12 +0400

    Public Subnets

--- main.tf
+++ main.tf
@@ -2,15 +2,57 @@ resource "aws_vpc" "VPC" {
  instance_tenancy     = "default"
  enable_dns_hostnames = true
   cidr_block          = var.vpc-cidr
+
   tags                = {
   Name                = "Trazler VPC"
   }
 }
+
+#********************************************************#
 
 resource "aws_internet_gateway" "IGW" {
   vpc_id = aws_vpc.VPC.id
 
   tags = {
     "Name" = "Trazler IGW"
+  }
+}
+
+#********************************************************#
+
+resource "aws_subnet" "PUBLIC-SUBNET-1a" {
+  vpc_id = aws_vpc.VPC.id
+  cidr_block = var.PUBLIC-SUBNET-1a-cidr-block
+  availability_zone = "eu-central-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+      Name = "Trazler PUBLIC-SUBNET-1a"
+  }
+}
+
+#********************************************************#
+
+resource "aws_subnet" "PUBLIC-SUBNET-1b" {
+  vpc_id = aws_vpc.VPC.id
+  cidr_block = var.PUBLIC-SUBNET-1b-cidr-block
+  availability_zone = "eu-central-1b"
+  map_public_ip_on_launch = true
+  
+  tags = {
+      Name = "Trazler PUBLIC-SUBNET-1b"
+  }
+}
+
+#********************************************************#
+
+resource "aws_subnet" "PUBLIC-SUBNET-1c" {
+  vpc_id = aws_vpc.VPC.id
+  cidr_block = var.PUBLIC-SUBNET-1c-cidr-block
+  availability_zone = "eu-central-1c"
+  map_public_ip_on_launch = true
+  
+  tags = {
+      Name = "Trazler PUBLIC-SUBNET-1c"
   }
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,27 @@ variable "vpc-cidr" {
   description = "VPC cidr-block"
   type = string
 }
+
+#********************************************************#
+
+variable "PUBLIC-SUBNET-1a-cidr-block" {
+  default = "10.0.0.0/24"
+  description = "PUBLIC SUBNET cidr block in AZ 1a"
+  type = string
+}
+
+#********************************************************#
+
+variable "PUBLIC-SUBNET-1b-cidr-block" {
+  default = "10.0.1.0/24"
+  description = "PUBLIC SUBNET cidr block in AZ 1b"
+  type = string
+}
+
+#********************************************************#
+
+variable "PUBLIC-SUBNET-1c-cidr-block" {
+  default = "10.0.2.0/24"
+  description = "PUBLIC SUBNET cidr block in AZ 1c"
+  type = string
+}

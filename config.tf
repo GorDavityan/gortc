3705aeef8e3a9b6d11252a79264e2ad649c90e05
Gor Davityan <gordavityan@Gors-MacBook-Air.local>
2022-05-08 13:08:39 +0400

    remote backend

--- config.tf
+++ config.tf
@@ -4,3 +4,10 @@ provider "aws" {
 
 #********************************************************#
 
+terraform {
+  backend "s3" {
+    bucket = "trazler-remote-backend"
+    key    = "state-files"
+    region = var.region
+  }
+}
